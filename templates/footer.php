1bd60b71a1971c66033b91536c54aecd493f2379
xicoofficial <contact@xicoofficial.co>
2014-06-08 23:53:24 -0400

    make it link to buy.php, history.php, logout.php, quote.php, and sell.php; Empower users to change their passwords; all donegit push -u origin master

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,22 @@
             </div>
 
+            <hr>
+            <br/><br/><br/><br/>
+            <hr>
+            <hr>
+
+            <ul class="menu">
+                <a href="index.php">  Portfolio  </a>-
+                <a href="quote.php">  Quote  </a>-
+                <a href="buy.php">  Buy  </a>-
+                <a href="sell.php">  Sell  </a>-
+                <a href="history.php">  History  </a>-
+                <a href="changepw.php">  Change Password  </a>-
+                <a href="logout.php"><strong>  Log Out</strong></a>-
+            </ul>
+
             <div id="bottom">
-                Copyright &#169; John Harvard
+                Copyright &#169; <a href="http://www.xicoofficial.co">XicoOfficial</a>
             </div>
 
         </div>
